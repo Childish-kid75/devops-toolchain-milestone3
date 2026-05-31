b9d123ade457afd9d216a18c6eebce0c8d13091c
Christiaan123701 <nelchristiaan05@gmail.com>
2026-05-31 15:09:13 +0000

    Nginx cookbook

--- chef/default.rb
+++ chef/default.rb
@@ -1,1 +1,21 @@
-# Chef recipe will go here
+#
+# Cookbook:: Milestone3_cookbook
+# Recipe:: default
+#
+# Copyright:: 2026, The Authors, All Rights Reserved.
+# install needed packages
+package 'nginx' do
+  action :install
+end
+
+# start nginx
+service 'nginx' do
+  action [:enable, :start]
+end
+
+
+# Display
+file '/var/www/html/index.html' do
+  content '<h1>Hello Sir this is my Nginx website</h1>'
+
+end
