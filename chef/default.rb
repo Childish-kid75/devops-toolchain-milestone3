27e2da10bd009d9a3e7baa49c878931417a49fa4
DariusGroenewald-601617 <601617@student.belgiumcampus.ac.za>
2026-05-31 20:54:38 +0200

    Update default.rb

--- chef/default.rb
+++ chef/default.rb
@@ -1,1 +1,20 @@
 # Chef recipe will go here
+#
+# Cookbook:: nginx_web
+# Recipe:: default
+#
+
+package 'nginx' do
+  action :install
+end
+
+service 'nginx' do
+  action [:enable, :start]
+end
+
+file '/usr/share/nginx/html/index.html' do
+  content '<html><body><h1>Hello from Chef!</h1><p>Nginx installed successfully.</p></body></html>'
+  owner 'root'
+  group 'root'
+  mode '0644'
+end
